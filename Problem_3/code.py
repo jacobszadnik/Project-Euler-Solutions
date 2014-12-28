3a914858d437ef8b28d64188c1dab0ac597ef3f8
Jacob Zadnik <jszadnik@titan.(none)>
2014-12-28 15:04:25 -0500

    Problem 3 has been completed.

--- Problem_3/code.py
+++ Problem_3/code.py
@@ -1,24 +1,26 @@
+import sys
+import math
 
-"""
-Problem Statement:
-The prime factors of 13195 are 5, 7, 13 and 29.
-What is the largest prime factor of the number 600851475143 ?
-"""
+def isPrime(x):
+    for m in range(2,int((math.sqrt(x)+1))):
+        if x%m==0:
+            return False
+    return True
 
-####################
-##  The largest prime factor of a number
-## will be equal to or less than the
-## square root of that number.
-####################
+targetNumber = 600851475143
 
-"""
-First we will find all the prime
-numbers less than the square root of
-our target number
-"""
-targetNumber = 600851475145
-sqRtOfTargetNumber = targetNumber**(1/2)
-primeNumbers []
-for x in range(2,int(sqRtOfTargetNumber)):
-    primeNumbers.append(x)
-currentPrime = 0
+listOfFactors = list()
+
+for x in range(1,int((math.sqrt(targetNumber)+1))):
+    m = targetNumber%x
+    if m==0:
+        listOfFactors.append(x)
+
+listOfFactors.reverse()
+
+for x in range(len(listOfFactors)):
+    if isPrime(listOfFactors[x]):
+        print "The answer is " + str(listOfFactors[x])
+        sys.exit()
+
+print "No solution found."
